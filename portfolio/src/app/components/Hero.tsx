91b242aeb0327f0d318aba0985a69c17b3c24f89
Syfer <alvinwcj@gmail.com>
2024-09-25 21:36:32 +0800

    added about text

--- portfolio/src/app/components/Hero.tsx
+++ portfolio/src/app/components/Hero.tsx
@@ -22,15 +22,30 @@ export const Hero = () => {
             <div className="flex flex-col gap-12 px-10">
                 {/* Main Hero Content  */}
                 <h1 className=" font-extrabold text-5xl tracking-wide ">
-                    Hi, I am Alvin and I am an Aspiring {" "}
+                    Hi, I&apos;m Alvin and I am an Aspiring {" "}
                     {/* Highlighted text */}
-                    <span className="block my-4 bg-clip-text text-transparent bg-gradient-to-b from-blue-400 to-blue-600">
+                    <span className="block mt-4 bg-clip-text text-transparent bg-gradient-to-b from-blue-400 to-blue-600">
                         Full-Stack Web Developer
                     </span>
                 </h1>
-                <p className=" text-lg text-wrap w-80 p-4 lg:w-[640px]">
-                    Passionate about exploring solutions to real world problems and developing ideas to turn into reality.
-                </p>
+                {/* About me text */}
+                <div className=" text-lg text-wrap w-80 p-2 lg:w-[640px] bg-gradient-to-b from-blue-400 to-blue-600 text-white rounded-lg theme_border shadow-custom-neumorphic">
+                    <p>
+                        I recently graduated with First Class Honours in Computer Science from the University of London, and I&apos;m currently based in Singapore. I&apos;m passionate about solving real-world challenges and turning innovative ideas into impactful solutions. My goal is to continually refine my web development skills and contribute to meaningful projects that can hopefully make a difference in people&apos;s lives.
+                    </p>
+
+                    
+                </div>
+                
+                {/* Employment status text */}
+                <div className="text-lg">
+                    <span className="font-bold mr-2">
+                        Employment Status:
+                    </span>  
+                    <span className="text-blue-600">
+                        Seeking for job opportunities...
+                    </span>
+                </div>
 
                 {/* CTA Button */}
                 <button className=" mt-8 py-4 px-8 w-64 rounded-full font-medium text-neu-blue text-xl bg-neu-white shadow-custom-neumorphic">
