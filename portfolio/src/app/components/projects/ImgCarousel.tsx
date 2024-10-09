897598fba2ab41aa65f4015e1b5fc6b2a9d2430a
Syfer <alvinwcj@gmail.com>
2024-10-09 16:45:53 +0800

    added image carousel

---
+++ portfolio/src/app/components/projects/ImgCarousel.tsx
@@ -0,0 +1,77 @@
+'use client'
+import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from "@heroicons/react/16/solid";
+import Image from "next/image";
+import { StaticImageData } from "next/image";
+import { useState } from "react";
+
+interface ImgProp {
+    src: StaticImageData;
+    alt: string;
+} 
+
+interface ImgCarouselProp {
+    images: ImgProp[]
+}
+
+const ImgCarousel = ( { images }: ImgCarouselProp ) => {
+
+    const [index, setIndex] = useState(0);
+
+    function handleNextSlide() {
+        setIndex( (prevIndex) => prevIndex === images.length-1 ? 0 : prevIndex + 1 )
+    }
+
+    function handlePrevSlide() {
+        setIndex( (prevIndex) => prevIndex === 0 ? images.length - 1 : prevIndex - 1)
+    }
+
+    return (
+        <div className="relative flex w-full h-full overflow-hidden">
+            
+            {/* image gallery */}
+            {images.map( (image) => (
+                <Image
+                    key={image.alt}
+                    src={image.src}
+                    alt={image.alt}
+                    className="object-cover block transition-transform ease-in-out duration-500"
+                    style={{transform: `translateX(-${100*index}%)`}}
+                />
+            ) )}
+            {/* left arrow icon */}
+            <button 
+                className="imgCarousel_arrow left-0"
+                onClick={handlePrevSlide}
+            >
+                <ArrowLeftCircleIcon 
+                    className="w-5"
+                />
+            </button>
+            
+            {/* right arrow icon */}
+            <button 
+                className="imgCarousel_arrow right-0"
+                onClick={handleNextSlide}
+            >
+                <ArrowRightCircleIcon 
+                    className="w-5"
+                />
+            </button>
+
+            <div className=" absolute bottom-2 flex gap-2 left-1/2 -translate-x-1/2">
+                {images.map((_, imageIndex) => (
+                    <button 
+                        key={imageIndex}
+                        className="p-1 rounded-full border-2 border-white focus-visible:p-3 focus-visible:rounded-none "
+                        style={{backgroundColor: `${imageIndex === index ? "black" : "grey"}`}}
+                        onClick={() => setIndex(imageIndex)}
+                    />
+                ))}
+            </div>
+            
+            
+        </div>
+    )
+}
+
+export default ImgCarousel
--- portfolio/src/app/components/projects/projectsData.ts
+++ portfolio/src/app/components/projects/projectsData.ts
@@ -1,13 +1,36 @@
 import HomeAPet from "../../assets/Projects/HomeAPet.jpg";
-
+import HomeAPetLandingpage from "../../assets/Projects/HomeAPet_landingpage.jpg"
+import HomeAPetGallery from "../../assets/Projects/HomeAPet_gallery.jpg"
+import HomeAPetRehomingForm from "../../assets/Projects/HomeAPet_rehomingform.jpg"
+import HomeAPetDashboard from "../../assets/Projects/HomeAPet_dashboard.jpg";
 
 export const projectsData = [
     {
         title: "HomeAPet",
         url: "https://homeapet.vercel.app/",
-        img: HomeAPet,
-        imgAlt: "HomeAPet landing page",
-        summary: "HomeAPet is a pet adoption website that allows users apply to adopt a pet or put up a pet for rehoming. This site was created with NextJS framework with self written custom API endpoints, login authentication and authorization, leveraging on ImgBB for image hosting while storing all the data in PostgreSQL hosted on Supabase, through Prisma.",
-        techStack: ["React", "NextJS", "Typescript","Tailwind","Prisma", "PostgreSQL", "Vercel", "Rest Api", ]
+        img: [
+            {
+                src: HomeAPet,
+                alt: "An image of HomeAPet Home Page"
+            }, 
+            {
+                src: HomeAPetLandingpage,
+                alt: "An image of HomeAPet features"
+            }, 
+            {
+                src: HomeAPetGallery,
+                alt: "An image of HomeAPet gallery"
+            }, 
+            {
+                src: HomeAPetRehomingForm,
+                alt: "An image of HomeAPet rehoming form"
+            },
+            {
+                src: HomeAPetDashboard,
+                alt: "An image of HomeAPet logged in dashboard"
+            }
+        ],
+        summary: "HomeAPet is a pet adoption website that allows users to create an account to apply for adopting a pet or put up a pet for rehoming. This site was created with NextJS framework with self written custom API endpoints, login authentication and authorization, leveraging on ImgBB for image hosting while storing all the data in PostgreSQL hosted on Supabase, through Prisma and lastly, styled with TailwindCSS.",
+        techStack: ["React", "NextJS", "Typescript","Tailwind","Prisma", "PostgreSQL", "Vercel", "Rest API", ]
     }
 ]
